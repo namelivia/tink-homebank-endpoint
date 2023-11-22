aa89286fc6a8dd905352e13ad01ee8d1c3e07fad
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2023-11-22 21:46:16 +0100

    Attempt to fix authentication

--- app/storage/storage.py
+++ app/storage/storage.py
@@ -1,4 +1,8 @@
 from tink_http_python.authentication.storage import Storage
+from tink_http_python.exceptions import (
+    NoRefreshTokenException,
+    NoAuthorizationCodeException,
+)
 
 
 class TokenStorage(Storage):
@@ -9,15 +13,14 @@ class TokenStorage(Storage):
     def store_new_refresh_token_refresh_token(self, new_refresh_token) -> None:
         self.refresh_token = new_refresh_token
 
-    # TODO: tink-http-python expects FileNotFound exceptions to be raised
     def retrieve_refresh_token(self) -> str:
         if self.refresh_token is None:
-            raise FileNotFoundError("Refresh token is not set")
+            raise NoRefreshTokenException("Refresh token is not set")
         return self.refresh_token
 
     def retrieve_authorization_code(self) -> str:
         if self.authorization_code is None:
-            raise FileNotFoundError("Authorization code is not set")
+            raise NoAuthorizationCodeException("Authorization code is not set")
         return self.authorization_code
 
     def store_new_authorization_code(self, new_authorization_code: str) -> str:
