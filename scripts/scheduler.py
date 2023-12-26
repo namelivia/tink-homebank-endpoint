8142fb4471acf6e40e4b0a5716569640669f4573
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2023-12-26 23:01:34 +0100

    Filter accounts, show only asset

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -16,13 +16,14 @@ def main():
     firefly_url = os.getenv("FIREFLY_URL")
     r = requests.get(f"{firefly_url}/api/v1/accounts", headers=headers)
     accounts = r.json()["data"]
+    accounts = list(
+        filter(lambda account: account["attributes"]["type"] == "asset", accounts)
+    )
     app_url = os.getenv("APP_URL")
     for account in accounts:
         date_until = account["attributes"]["current_balance_date"].split("T")[0]
         link = f"{app_url}/update?date_until={date_until}&account_id={account['id']}"
-        Notifications.send(
-            f"{account['attributes']['active']}{account['attributes']['name']}: {link}"
-        )
+        Notifications.send(f"{account['attributes']['name']}: {link}")
 
 
 if __name__ == "__main__":
