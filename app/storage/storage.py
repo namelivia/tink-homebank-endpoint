c7ea9950630bc58534675dc72625553c8eba05dc
José Ignacio Amelivia Santiago <hello@namelivia.com>
2024-08-22 17:13:03 +0200

    Last update

--- app/storage/storage.py
+++ app/storage/storage.py
@@ -1,6 +1,6 @@
 from tink_http_python.authentication.storage import Storage
 from tink_http_python.exceptions import (
-    NoRefreshTokenException,
+    NoAccessTokenException,
     NoAuthorizationCodeException,
 )
 import logging
@@ -11,6 +11,7 @@ logger = logging.getLogger(__name__)
 class TokenStorage(Storage):
     def __init__(self):
         self.authorization_code = None
+        self.access_token = None
 
     def retrieve_authorization_code(self) -> str:
         if self.authorization_code is None:
@@ -19,3 +20,11 @@ class TokenStorage(Storage):
 
     def store_new_authorization_code(self, new_authorization_code: str) -> str:
         self.authorization_code = new_authorization_code
+
+    def retrieve_access_token(self) -> str:
+        if self.access_token is None:
+            raise NoAccessTokenException("Access token is not set")
+        return self.access_token
+
+    def store_new_access_token(self, new_access_token: str) -> str:
+        self.access_token = new_access_token
